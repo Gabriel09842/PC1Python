37cde1db9d83bf36f5aad891459aa2129cc7ac22
Gabriel09842 <157264815+Gabriel09842@users.noreply.github.com>
2024-01-25 00:25:36 +0000

    Ejercicios PC1

--- Ejercicio_8.py
+++ Ejercicio_8.py
@@ -9,14 +9,12 @@
 
 hora = input('Ingresa una hora del día: ')
 n = hora.index(':')
-hora_numerica = int(hora[:n])
+hora_num1 = int(hora[:n])
+hora_num2 = int(hora[n+1:])
 
-if 7 <= hora_numerica <= 8:
+if (7 <= hora_num1 < 8) or (hora_num1 == 8 and hora_num2 == 00):
     print("Es hora de desayunar.")
-elif 12 <= hora_numerica <= 13:
+elif (12 <= hora_num1 < 13) or (hora_num1 == 13 and hora_num2 == 00):
     print("Es hora de almorzar.")
-elif 18 <= hora_numerica <= 19:
+elif (18 <= hora_num1 < 19) or (hora_num1 == 19 and hora_num2 == 00):
     print("Es hora de cenar.")
-else:
-    print('Hora no válida')
-
